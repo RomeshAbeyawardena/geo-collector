a924ca32f1f5cd27eeec017de4a98c37bd887e73
Romesh Abeyawardena <romesh.a@outlook.com>
2025-11-08 12:32:54 +0000

    Implemented queue consumer

--- server/index.ts
+++ server/index.ts
@@ -4,4 +4,8 @@ export default {
 		const r = new router(env, ctx);
 		return await r.handle(request);
 	},
+	async queue(batch, env: Env) : Promise<void> {
+		const messages = batch.messages;
+		console.log(messages);
+	}
 } satisfies ExportedHandler<Env>;
